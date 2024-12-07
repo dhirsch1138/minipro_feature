a4085bb36bf5c3d0df7cada0ab0ee5f251db5cb9
David Hirsch <dhirsch1138@gmail.com>
2024-12-07 00:58:35 +0000

    Have to install packages from the install script to get them to show in the parent. How odd.

--- src/minipro/install.sh
+++ src/minipro/install.sh
@@ -1,6 +1,6 @@
-# sudo apt update
-# sudo apt upgrade -yq
-# sudo apt install libusb-dev libusb-1.0-0-dev fakeroot debhelper dpkg-dev libb64-dev udev git pkg-config usbutils -yq
+sudo apt update
+sudo apt upgrade -yq
+sudo apt install libusb-dev libusb-1.0-0-dev fakeroot debhelper dpkg-dev libb64-dev udev git pkg-config usbutils -yq
 mkdir .tempbuild
 cd .tempbuild
 git clone https://gitlab.com/DavidGriffith/minipro.git
